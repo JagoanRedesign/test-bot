1ce774b57473114b969b7d054de10edf77a67d51
Sandy Ken <tokairobo@gmail.com>
2023-04-24 20:39:32 +0700

    change some variable name and moving port into .env files

--- index.js
+++ index.js
@@ -3,18 +3,19 @@ require("dotenv").config()
 const express = require("express")
 const { Bot, webhookCallback } = require("grammy")
 
-const tokens = process.env.BOT_TOKEN
 const domain = process.env.DOMAIN
+const port = process.env.PORT
+const token = process.env.BOT_TOKEN
 
-const bot = new Bot(tokens)
+const bot = new Bot(token)
 const app = express()
 
 app.use(express.json())
-app.use(`/${ tokens }`, webhookCallback(bot, "express"))
+app.use(`/${ token }`, webhookCallback(bot, "express"))
 
 bot.command("start", ctx => ctx.reply("hello world yaa"))
 
-app.listen(5000, async () => {
+app.listen(port, async () => {
   let options = { drop_pending_updates: true }
-  await bot.api.setWebhook(`https://${ domain }/${ tokens }`, options)
+  await bot.api.setWebhook(`https://${ domain }/${ token }`, options)
 })
