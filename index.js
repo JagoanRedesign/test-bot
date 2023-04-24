d2580be7f3eeb8ff58d5bf099bd87e063af5822d
Sandy Ken <tokairobo@gmail.com>
2023-04-25 00:05:48 +0700

    modifying file structure

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 require("dotenv").config()
 
+const express = require("express")
 const { Bot, webhookCallback } = require("grammy")
 
+const port = process.env.PORT
 const token = process.env.BOT_TOKEN
+
 const bot = new Bot(token)
+const app = express()
+
+app.use(express.json())
+app.use(`/${ token }`, webhookCallback(bot, "express"))
 
 bot.command("start", ctx => ctx.reply("hello world yaa"))
 
-module.exports = webhookCallback(bot, "http")
+app.listen(port, () => true)
