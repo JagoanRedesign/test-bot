6041c171fc231f443ec6705c94a0cdaeb6c0a17e
Sandy Ken <tokairobo@gmail.com>
2023-04-24 23:50:09 +0700

    modifying file structure

---
+++ api/index.js
@@ -0,0 +1,11 @@
+
+require("dotenv").config()
+
+const { Bot, webhookCallback } = require("grammy")
+
+const token = process.env.BOT_TOKEN
+const bot = new Bot(token)
+
+bot.command("start", ctx => ctx.reply("hello world yaa"))
+
+module.exports = webhookCallback(bot, "http")
