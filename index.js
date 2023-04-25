1055628d87790ec019e3cb3bd4e5a4b6ac817d27
Sandy Ken <tokairobo@gmail.com>
2023-04-25 08:22:47 +0700

    shorten code

--- index.js
+++ index.js
@@ -14,21 +14,16 @@ app.use(express.json())
 app.use(`/${ token }`, webhookCallback(bot, "express"))
 
 bot.command("start", ctx => ctx.reply("hello world yaa"))
-bot.command("video", ctx => {
+bot.command("video", async ctx => {
   let url = ctx.message.text.replace("/video").trim()
   let endpoint = `https://dlpanda.com?url=${ url }&token=G7eRpMaa`
   let headers = { headers: { "User-Agent": "Postify/1.0.0" } }
-  axios.get(endpoint, headers)
-    .then(response => {
-      let text = `${ response.status } ${ response.statusText }`
-      ctx.reply(text)
-      return true
-    })
-    .catch(err => {
-      let text = `${ err.response.status } ${ err.response.statusText }`
-      ctx.reply(text)
-      return true
-    })
+  try {
+    let response = await axios.get(endpoint, headers)
+    return ctx.reply(`${ response.status } ${ response.statusText }`)
+  } catch ( err ) {
+    return ctx.reply(`${ err.response.status } ${ err.response.statusText }`)
+  }
 })
 
 app.listen(port, () => console.log("deployed!!"))
