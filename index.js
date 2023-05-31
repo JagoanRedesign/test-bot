1228e503c7e5a4b59ebcfde6920ed7a1fc90f223
Sandy Ken <tokairobo@gmail.com>
2023-05-31 18:02:35 +0700

    testing rate limiter

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 require("dotenv").config()
 
+const { freeStorage } = require("@grammyjs/storage-free")
 const express = require("express")
-const axios = require("axios")
-const { Bot, webhookCallback } = require("grammy")
+const { Bot, webhookCallback, session } = require("grammy")
+
 
 const port = process.env.PORT
 const token = process.env.BOT_TOKEN
+const limit = { message: 2, time: 4000 }
 
 const bot = new Bot(token)
 const app = express()
@@ -13,17 +15,57 @@ const app = express()
 app.use(express.json())
 app.use(`/${ token }`, webhookCallback(bot, "express"))
 
-bot.command("start", ctx => ctx.reply("hello world yaa"))
-bot.command("video", async ctx => {
-  let url = ctx.message.text.replace("/video").trim()
-  let endpoint = `https://dlpanda.com?url=${ url }&token=G7eRpMaa`
-  let headers = { headers: { "User-Agent": "Mozilla/5.0 (Linux; Android 10; SM-G996U Build/QP1A.190711.020; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Mobile Safari/537.36" } }
-  try {
-    let response = await axios.get(endpoint, headers)
-    return ctx.reply(`${ response.status } ${ response.statusText }`)
-  } catch ( err ) {
-    return ctx.reply(`${ err.response.status } ${ err.response.statusText }`)
+bot.use(session({
+  initial: () => ({ last_message_date: null, count: 1 }),
+  storage: freeStorage(bot.token)
+}))
+
+bot.use(async ( ctx, next ) => {
+  let session = ctx.session
+
+  let currentTime =  ctx.message
+    ? ctx.message.date * 1000 : ctx.callbackQuery
+    ? ctx.callbackQuery.date * 1000 : null
+
+  if ( !currentTime ) {
+    return await next()
   }
+
+  if ( !session.last_message_date ) {
+    let source = {
+      last_message_date: currentTime.toString(),
+      count: 1
+    }
+
+    Object.assign(ctx.session, source)
+    await ctx.reply("initialize rate limit session!")
+    return await next()
+  }
+
+  let cachedCount = session.count
+  let cachedTimes = parseInt(session.last_message_date)
+  let gap = currentTime - cachedTimes
+
+  if ( cachedCount >= limit.message && gap <= limit.time ) {
+    return await ctx.reply("Too many requests!!!")
+  }
+
+  let source = {
+    last_message_date: currentTime.toString()
+  }
+
+  if ( gap <= limit.time ) {
+    source.count = cachedCount + 1
+    await ctx.reply(`add count : ${ source.count }`)
+  } else {
+    source.count = cachedCount - 1 < 1 ? 1 : cachedCount - 1
+    await ctx.reply(`del count : ${ source.count }`)
+  }
+
+  Object.assign(ctx.session, source)
+  await next()
 })
 
-app.listen(port, () => console.log("deployed!!"))
+bot.command("start", ctx => ctx.reply("started"))
+
+app.listen(port)
